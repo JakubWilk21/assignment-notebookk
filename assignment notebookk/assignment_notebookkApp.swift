30dae70dbedffa1f963dbb9dfd205e21706d178e
Jakub <Wilk>
2023-04-30 21:38:58 -0500

    Initial Commit

---
+++ assignment notebookk/assignment_notebookkApp.swift
@@ -0,0 +1,17 @@
+//
+//  assignment_notebookkApp.swift
+//  assignment notebookk
+//
+//  Created by Jakub Wilk on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct assignment_notebookkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
